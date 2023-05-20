d559e2f18515c7e1984eb2321f0a6ee9cf56d45e
jihwan21 <96354328+jihwan21@users.noreply.github.com>
2023-05-20 12:09:05 +0900

    Update Jihwan.java

--- Jihwan.java
+++ Jihwan.java
@@ -2,8 +2,26 @@ package test;
 
 import java.util.Scanner;
 
-class Monster {
+class Time {
+	int time = 1;
+
+	void count() {
+		if (time == 12)
+			time = 0;
+		time += 1;
+	}
+
+	int current_time() {
+		return time;
+	}
+}
+
+class Monster extends Time {
 	String name;
+	String skill_name;
+	int skill_power;
+
+	int Nocturnality; // 1 : 야행성, 0 : 주행성
 	int hp;
 	int hpMax;
 	int mp;
@@ -11,9 +29,12 @@ class Monster {
 	int power;
 	int defense;
 	int experience;
+	int money;
 
-	public Monster(String name, int hp, int hpMax, int mp, int mpMax, int power, int defense, int experience) {
+	public Monster(String name, int Nocturnality, int hp, int hpMax, int mp, int mpMax, int power, int defense,
+			int experience, int money) {
 		this.name = name;
+		this.Nocturnality = Nocturnality;
 		this.hp = hp;
 		this.hpMax = hpMax;
 		this.mp = mp;
@@ -21,6 +42,28 @@ class Monster {
 		this.power = power;
 		this.defense = defense;
 		this.experience = experience;
+	}
+
+	int current_power() { // 현재 time에 맞는 power return > 전투 공격력 산출 시 활용
+		if (super.current_time() <= 6) { // time이 낮인 경우
+			if (Nocturnality == 0) { // 낮이면서 주행성 동물
+				power *= 1.1;
+			} else if (Nocturnality == 1) { // 낮이면서 야행성 동물
+				power *= 0.9;
+			}
+		} else if (super.current_time() > 6) { // time이 밤인 경우
+			if (Nocturnality == 0) { // 밤이면서 주행성 동물
+				power *= 0.9;
+			} else if (Nocturnality == 1) { // 밤이면서 야행성 동물
+				power *= 1.1;
+			}
+		}
+		return power;
+	}
+
+	void setSkill(String name) {
+		this.skill_name = name;
+		this.skill_power = power * 2;
 	}
 }
 
@@ -52,7 +95,6 @@ class ClassArray {
 	int mpMax;
 	int power;
 	int defense;
-	int experience;
 
 	public ClassArray(Achievement obj, Monster obj_) {
 		this.name = obj.achievement_name;
@@ -63,7 +105,6 @@ class ClassArray {
 		this.mpMax = obj_.mpMax;
 		this.power = obj_.power;
 		this.defense = obj_.defense;
-		this.experience = obj_.experience;
 
 	}
 
@@ -76,6 +117,24 @@ class ClassArray {
 		Player.mp += mp;
 		Player.mpMax += mpMax;
 //		Player.upgradeS01 = upgradeS01;
+	}
+}
+
+class Position_Monster {
+	String position_name; // 지역 이름(한강, 북한산국립공원 등)
+	Monster stage_1;
+	Monster stage_2;
+	Monster stage_3;
+	Monster stage_4;
+	Monster stage_5;
+
+	Position_Monster(String name, Monster a, Monster b, Monster c, Monster d, Monster e) {
+		this.position_name = name;
+		this.stage_1 = a;
+		this.stage_2 = b;
+		this.stage_3 = c;
+		this.stage_4 = d;
+		this.stage_5 = e;
 	}
 }
 
@@ -99,12 +158,34 @@ public class Jihwan {
 		Scanner in = new Scanner(System.in);
 
 //		Player player = new Player();
-		Monster tiger = new Monster("tiger", 1000, 1000, 200, 200, 500, 500, 1000);
-		Monster bear = new Monster("bear", 1000, 1000, 200, 200, 500, 500, 1000);
-		Monster eagle = new Monster("eagle", 1000, 1000, 200, 200, 500, 500, 1000);
+		Monster tiger = new Monster("tiger", 1, 1000, 1000, 200, 200, 500, 500, 1000, 500);
+		Monster bear = new Monster("bear", 0, 1000, 1000, 200, 200, 500, 500, 1000, 500);
+		Monster eagle = new Monster("eagle", 0, 1000, 1000, 200, 200, 500, 500, 1000, 500);
+		Monster goldfish = new Monster("goldfish", 0, 1000, 1000, 200, 200, 500, 500, 1000, 500);
+		Monster ant = new Monster("ant", 0, 1000, 1000, 200, 200, 500, 500, 1000, 500);
 		Achievement tiger_ach = new Achievement(tiger);
 		Achievement bear_ach = new Achievement(bear);
 		Achievement eagle_ach = new Achievement(eagle);
+		// time test
+		Time ti = new Time();
+		ti.count(); // time + 1
+		System.out.println("현재 시간 : " + ti.current_time());
+		System.out.println("현재 tiger power : " + tiger.current_power()); // 야행성으로 설정해서 500 -> 450
+		System.out.println("현재 bear power : " + bear.current_power()); // 주행성으로 설정해서 500 -> 550
+		System.out.println();
+
+		// 스킬 설정 및 출력 테스트
+		tiger.setSkill("목덜미 물기");
+		System.out.println("스킬 이름 : " + tiger.skill_name);
+		System.out.println("스킬 공격력 : " + tiger.skill_power);
+		System.out.println();
+
+		// stage에 해당하는 몬스터 객체 리턴 테스트
+		Position_Monster Hangang = new Position_Monster("한강", tiger, bear, eagle, goldfish, ant);
+		System.out.println("스테이지 1 몬스터 : " + Hangang.stage_1.name);
+		System.out.println("스테이지 1 몬스터 : " + Hangang.stage_1.skill_name);
+		System.out.println();
+
 		// 업적 카운트 올리기(테스트용)
 		for (int i = 0; i < 6; i++) {
 			tiger_ach.count();
