7d1913821ca28917ddd12556d6f81a38a07cc923
jihwan21 <96354328+jihwan21@users.noreply.github.com>
2023-05-28 22:26:58 +0900

    Update Strategy.java

--- TeamProject/Strategy.java
+++ TeamProject/Strategy.java
@@ -13,44 +13,40 @@ class Strategy_Attack extends Player {
 	String s4 = "전략적 선택지 4번 : 50% 확률로 1턴 동안 공격력이 2배 증가 or 절반 감소";
 
 	public Strategy_Attack() {
-		System.out.println("<공격 전략전 선택지 목록>");
+		System.out.println("----- <공격 전략적 선택지 목록> -----");
 		System.out.println(s1);
 		System.out.println(s2);
 		System.out.println(s3);
 		System.out.println(s4);
 	}
 
-	@SuppressWarnings("static-access")
-	public int Sattack(int select) {
+	public int Select_attack(int select) {
+		int S_power = p.current_power(); // 기존 플레이어 공격력 유지하기 위해 새로운 변수 생성하여 공격력 증가
 		if (select == 1) {
-			System.out.println("1번 전략을 선택하였습니다. 다음 1턴 동안 공격력이 2배 증가한다.");
-			int S_power = p.power; // 기존 플레이어 공격력 유지하기 위해 새로운 변수 생성하여 공격력 증가
+			System.out.println("1번 전략을 선택하였습니다. 다음 1턴 동안 공격력이 2배 증가합니다.");
 			S_power *= 2; // 공격력 2배 증가
-			return S_power; // 2배 증가된 공격력을 리턴하여 최종 공격 산출식에 활용 or 스킬 공격력 계산에 활용하는 방식으로 적용
-			//
 		} else if (select == 2) {
 			System.out.println("2번 전략을 선택하였습니다. 다음 1턴 동안 공격력이 3배 증가합니다.");
-			int S_power = p.power;
 			S_power *= 3; //
-			return S_power;
 		} else if (select == 3) {
-			System.out.println("3번 전략을 선택하였습니다. 25% 확률로 1턴 동안 공격력이 5배 증가합니다!");
-			int S_power = p.power;
+			System.out.println("3번 전략을 선택하였습니다. 25% 확률로 1턴 동안 공격력이 5배 증가합니다.");
 			if (randomNumber < 25) {
+				System.out.println(">> 공격력이 5배 증가합니다!!!");
 				S_power *= 5;
+			} else {
+				System.out.println(">> 아무런 변화가 없었습니다...");
 			}
-			return S_power;
 		} else if (select == 4) {
 			System.out.println("4번 전략을 선택하였습니다. 50% 확률로 1턴 동안 공격력이 2배 증가하거나 절반 감소합니다.");
-			int S_power = p.power;
 			if (randomNumber < 50) {
+				System.out.println(">> 공격력이 2배 증가합니다!!!");
 				S_power *= 2;
 			} else {
+				System.out.println(">> 공격력이 절반 감소합니다...");
 				S_power *= 0.5;
 			}
-			return S_power;
 		}
-		return 0;
+		return S_power;
 	}
 }
 
@@ -65,18 +61,17 @@ class Strategy_Defense extends Player {
 	String s4 = "전략적 선택지 4번 : 10% 확률로 1턴 동안 방어력이 10배 증가";
 
 	public Strategy_Defense() {
-		System.out.println("<방어 전략전 선택지 목록>");
+		System.out.println("----- <방어 전략전 선택지 목록> -----");
 		System.out.println(s1);
 		System.out.println(s2);
 		System.out.println(s3);
 		System.out.println(s4);
 	}
 
-	@SuppressWarnings("static-access")
-	public int Sdefense(int select) {
+	public int Select_defense(int select) {
+		int S_defense = p.defense;
 		if (select == 1) {
 			System.out.println("1번 전략을 선택하였습니다. 다음 1턴 동안 방어력이 2배 증가합니다.");
-			int S_defense = p.defense;
 			S_defense *= 2;
 			return S_defense;
 		} else if (select == 2) {
@@ -89,18 +84,23 @@ class Strategy_Defense extends Player {
 		} else if (select == 3) {
 			System.out.println("3번 전략을 선택하였습니다. 25% 확률로 hp 50% 회복합니다.");
 			if (randomNumber < 25) {
+				System.out.println(">> hp를 50% 회복!!!");
 				if (p.hp > p.hpMax * 0.5) {
 					p.hp = p.hpMax;
 				} else {
 					p.hp += p.hpMax * 0.5;
 				}
+			} else {
+				System.out.println(">> 아무런 변화가 없었습니다...");
 			}
 		} else if (select == 4) {
 			System.out.println("4번 전략을 선택하였습니다. 10% 확률로 1턴 동안 방어력이 10배 증가합니다.");
 			if (randomNumber < 10) {
-				int S_defense = p.defense;
+				System.out.println(">> 방어력이 10배 증가합니다!!!");
 				S_defense *= 10;
 				return S_defense;
+			} else {
+				System.out.println(">> 아무런 변화가 없었습니다...");
 			}
 		}
 		return 0;
