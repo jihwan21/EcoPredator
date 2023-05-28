71886c1a7a9206ef50181cd3e2ef710997ba6e11
Park-jongsin <66291287+Park-jongsin@users.noreply.github.com>
2023-05-29 00:46:22 +0900

    Update Time.java

--- TeamProject/Time.java
+++ TeamProject/Time.java
@@ -1,16 +1,18 @@
 package TeamProject;
 
-//시간 클래스
 class Time {
 	int time = 1;
+	static int allTime = 1;
 
 	void count() {
 		if (time == 12)
 			time = 0;
 		time += 1;
+		allTime++;
 	}
 
 	int current_time() {
 		return time;
 	}
 }
+
