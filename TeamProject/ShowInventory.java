3001f37e2c623641a8a4318a4a780f77f76dc6b1
Park-jongsin <66291287+Park-jongsin@users.noreply.github.com>
2023-05-29 23:56:26 +0900

    Update ShowInventory.java

--- TeamProject/ShowInventory.java
+++ TeamProject/ShowInventory.java
@@ -6,14 +6,13 @@ import javax.swing.JTable;
 
 class ShowInventory extends JFrame {
 	JTable table;
-	JScrollPane scroll; // 테이블 위에 열 라벨을 넣어주자~ scroll
-	String[][] data; // 3명의 정보를 담을 2차원 배열을 생성한다.
+	JScrollPane scroll; // 테이블 위에 열 라벨 넣기
+	String[][] data; // 2차원 배열을 생성
 	String[] title = { "구분", "개수" }; // 제목
 
 	public ShowInventory() {
-		// 플레이어 클래스 필수 -> 플레이어 클래스로 객체 생성-> 사실상 필드멤버를 복사
-		data = new String[9][3];
-//hpPotionN, mpPotionN, 수류탄N, 일반장비N, 아가미N, 어항N, 날개N, pMoney;
+		data = new String[9][3];//9x3테이블 생성
+		//hpPotionN, mpPotionN, 수류탄N, 일반장비N, 아가미N, 어항N, 날개N, pMoney;
 		data[0][0] = "투구";
 		if (Player.투구N == 0)
 			data[0][1] = "없음";
@@ -45,7 +44,7 @@ class ShowInventory extends JFrame {
 			data[4][1] = "보유중";
 
 		data[5][0] = "HP포션";
-		data[5][1] = Integer.toString(Player.hpPotionN);
+		data[5][1] = Integer.toString(Player.hpPotionN);//플레이어 클래스의 전역 필드 hpPotionN을 사용
 
 		data[6][0] = "MP포션";
 		data[6][1] = Integer.toString(Player.mpPotionN);
@@ -61,13 +60,13 @@ class ShowInventory extends JFrame {
 		scroll = new JScrollPane(table);
 		add(scroll);
 
-		table.getColumn("구분").setPreferredWidth(80);
+		table.getColumn("구분").setPreferredWidth(80);//크기 조정
 		table.getColumn("개수").setPreferredWidth(100);
 //		table.getColumn(Player.name).setPreferredWidth(75);
 //Player.style + " " + Player.p.tribe + " " + Player.name,
-		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);//창닫기
 		pack();
-		setSize(180, 220);
+		setSize(180, 220);//전체 크기 조정
 		setVisible(true);
 	}
 }
