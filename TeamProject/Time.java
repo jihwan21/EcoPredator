523a0d7c9f9d9d11025eee74f904ef2dfaa1a408
jihwan21 <96354328+jihwan21@users.noreply.github.com>
2023-05-29 23:18:39 +0900

    Update Time.java

--- TeamProject/Time.java
+++ TeamProject/Time.java
@@ -1,14 +1,18 @@
 package TeamProject;
 
+//시간 클래스
 class Time {
 	int time = 1;
 	static int allTime = 1;
-	
+
 	void count() {
-		if (time == 12){
-			if (Exploration.Seoul_clear == 1 || Exploration.Gangwon_clear == 1||Chungcheong_clear == 1 ||  Jeolla_clear == 1 || Gyeongsang_clear == 1) {
-		    		Player.pMoney += 1000 * Exploration.Seoul_clear + 2000 * Exploration.Gangwon_clear + 3000 * Chungcheong_clear + 4000* Jeolla_clear + 5000 * Gyeongsang_clear;
-        		 }	
+		if (time == 12) {
+			if (Exploration.Seoul_clear == 1 || Exploration.Gangwon_clear == 1 || Exploration.Chungcheong_clear == 1
+					|| Exploration.Jeolla_clear == 1 || Exploration.Gyeongsang_clear == 1) {
+				Player.pMoney += 1000 * Exploration.Seoul_clear + 2000 * Exploration.Gangwon_clear
+						+ 3000 * Exploration.Chungcheong_clear + 4000 * Exploration.Jeolla_clear
+						+ 5000 * Exploration.Gyeongsang_clear;
+			}
 			time = 0;
 		}
 		time += 1;
@@ -19,4 +23,3 @@ class Time {
 		return time;
 	}
 }
-
