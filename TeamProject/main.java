ec62f19ece2f8677515a505cc5a1e3abbac95690
jihwan21 <96354328+jihwan21@users.noreply.github.com>
2023-05-29 23:17:00 +0900

    Update main.java

--- TeamProject/main.java
+++ TeamProject/main.java
@@ -9,14 +9,14 @@ public class main {
 
 	public static void main(String[] args) { // game start
 		Scanner in = new Scanner(System.in);
-		
+
 		Print.printMain();
 		// 캐릭터 생성 및 선택
 		System.out.print("캐릭터 이름을 작성하세요: ");
 		Player.name = in.next();
-		
+
 		System.out.println("플레이어의 이름은 \"" + Player.name + "\"입니다.");
-		
+
 		System.out.println("초기 캐릭터");
 		System.out.println("1. 모기");
 		System.out.println("2. 하루살이");
@@ -26,29 +26,33 @@ public class main {
 		System.out.print("원하는 초기 캐릭터를 선택하세요. : ");
 		int char_choice = in.nextInt();
 		System.out.println("");
-		
-		if(char_choice == 1) {
-			System.out.println("모기를 선택했습니다.\n");
-			Player.p = new Mosquito(3,1);
-		}
-		else if(char_choice == 2) {
-			System.out.println("하루살이를 선택했습니다.\n");
-			Player.p = new Mayfly(3,1);
-		}
-		else if(char_choice == 3) {
-			System.out.println("바퀴벌레를 선택했습니다.\n");
-			Player.p = new Roach(3,1);
-		}
-		else if(char_choice == 4) {
-			System.out.println("나비를 선택했습니다.\n");
-			Player.p = new Butterfly(3,1);
-		}
-		else if(char_choice == 5) {
-			System.out.println("꿀벌을 선택했습니다.\n");
-			Player.p = new Honeybee(3,1);
+
+		if (char_choice == 1) {
+			System.out.println("모기를 선택했습니다.");
+			Player.p = new Mosquito(3, 1);
+		} else if (char_choice == 2) {
+			System.out.println("하루살이를 선택했습니다.");
+			Player.p = new Mayfly(3, 1);
+		} else if (char_choice == 3) {
+			System.out.println("바퀴벌레를 선택했습니다.");
+			Player.p = new Roach(3, 1);
+		} else if (char_choice == 4) {
+			System.out.println("나비를 선택했습니다.");
+			Player.p = new Butterfly(3, 1);
+		} else if (char_choice == 5) {
+			System.out.println("꿀벌을 선택했습니다.");
+			Player.p = new Honeybee(3, 1);
 		}
 
 		while (true) {
+			if (Exploration.Jeju_clear == 1) {
+				System.out.println("??? 지역에 입장했습니다.");
+				System.out.println("??? 영역에 들어왔습니다.");
+				Human m1 = new Human();
+				Battle.battleEnding(m1);
+				System.exit(0);
+			}
+
 			Print.printMain();// =========================================================================================메인
 								// 배경 프린트
 			time.count(); // 시간 turn count
@@ -88,7 +92,7 @@ public class main {
 				break;
 
 			case 6:
-				ach.process();
+				ach.process(); // 진화
 				break;
 
 			case 0:
