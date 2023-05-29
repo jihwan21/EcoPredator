98998cd6fed0345da249b5781b50697c8d8f6cfb
Park-jongsin <66291287+Park-jongsin@users.noreply.github.com>
2023-05-29 22:48:36 +0900

    Update Time.java

--- TeamProject/Time.java
+++ TeamProject/Time.java
@@ -3,10 +3,14 @@ package TeamProject;
 class Time {
 	int time = 1;
 	static int allTime = 1;
-
+	
 	void count() {
-		if (time == 12)
+		if (time == 12){
+			if (Exploration.Seoul_clear == 1 || Exploration.Gangwon_clear == 1||Chungcheong_clear == 1 ||  Jeolla_clear == 1 || Gyeongsang_clear == 1) {
+		    		Player.pMoney += 1000 * Exploration.Seoul_clear + 2000 * Exploration.Gangwon_clear + 3000 * Chungcheong_clear + 4000* Jeolla_clear + 5000 * Gyeongsang_clear;
+        		 }	
 			time = 0;
+		}
 		time += 1;
 		allTime++;
 	}
